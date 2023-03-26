cb584d6cbd420765658a74b97c8ac6b9ecbb88ca
shahazad08 <sk.shahazad@gmail.com>
2023-03-26 12:57:37 +0530

    Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './routing/aboutus/aboutus.component';
+import { ContactusComponent } from './routing/contactus/contactus.component';
+import { HomeComponent } from './routing/home/home.component';
+import { PageNotFoundComponent } from './routing/page-not-found/page-not-found.component';
+import { UserComponent } from './routing/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'about',
+    component:AboutusComponent
+  },
+  {
+    path:'contact',
+    component:ContactusComponent
+  },
+  {
+    path:'user/:id',
+    component:UserComponent
+  },
+  {
+    path:'**',
+    component:PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './routing/home/home.component';
+import { AboutusComponent } from './routing/aboutus/aboutus.component';
+import { ContactusComponent } from './routing/contactus/contactus.component';
+import { DemoComponentComponent } from './demo-component/demo-component.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UserComponent } from './routing/user/user.component';
+import { PageNotFoundComponent } from './routing/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutusComponent,
+    ContactusComponent,
+    DemoComponentComponent,
+    UserComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
